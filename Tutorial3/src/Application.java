ca4253f552e3a0a5b0cfcfb7cb2fc5ebeaa996ba
Margareth Borba <mborba1@icloud.com>
2022-01-25 12:35:44 -0500

    initial commit

---
+++ Tutorial3/src/Application.java
@@ -0,0 +1,18 @@
+
+public class Application {
+	public static void main(String[] args) {
+	   int myInt =7;
+	   
+	   String text = "Hello";
+	   
+	   String blank = " ";
+	   
+	   String name = "Bob";
+	   
+	   String greeting = text + blank + name;
+	   
+	   System.out.println(greeting);
+	   
+	   System.out.println("My integer is: " + myInt);
+	}
+}
