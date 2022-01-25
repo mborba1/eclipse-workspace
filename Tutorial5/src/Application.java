4a71379e14c17aef4e3332893aa5de9a0d7a95bd
Margareth Borba <mborba1@icloud.com>
2022-01-25 13:08:01 -0500

    input/output

--- Tutorial5/src/Application.java
+++ Tutorial5/src/Application.java
@@ -1,11 +1,44 @@
+import java.util.Scanner;
 
 public class Application {
 	public static void main(String[] args) {
-	  for(int i =0; i < 5; i++) {
+//	  for(int i =0; i < 5; i++) {
+//		
+////		System.out.println("Hello");
+//		System.out.printf("The value of i is:  %d\n", i);
+////		System.out.println();
+//	  }
 		
-//		System.out.println("Hello");
-		System.out.printf("The value of i is:  %d\n", i);
-//		System.out.println();
-	  }
+		//conditionals
+		
+//		int myInt = 20;
+//		
+//		if(myInt < 20) {
+//			System.out.println("Yes, it's true!");
+//		}
+//		else if(myInt > 20) {
+//			System.out.println("No, it's false!");
+//		}
+//		else {
+//			System.out.println("None of the above.");
+//		}
+		
+		//getting user input
+		//create scanner object
+		Scanner input = new Scanner(System.in);
+		
+		//output the prompt
+		System.out.println("Enter a floating point value: ");
+		
+		//wait for the user to enter a new line of text
+		//String line = input.nextLine();
+		
+		//wait for user input using integer
+		//int value = input.nextInt();
+		
+		double value = input.nextDouble();
+		
+		//Tell them what they entered.
+		System.out.println("You entered: " + value);
 	}
 }
