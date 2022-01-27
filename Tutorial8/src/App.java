1ce81109efd4ae6e34b9b523c88b1468d05c9b36
Margareth Borba <mborba1@icloud.com>
2022-01-27 15:41:36 -0500

    tutorial 8

---
+++ Tutorial8/src/App.java
@@ -0,0 +1,49 @@
+
+class Person {
+	//Instance variables (data or "state")
+	
+	String name;
+	int age;
+	
+	//classes can contain
+	//1.Data
+	//2.Subsroutines (methods)
+	
+	void speak() {
+		System.out.println("My name is: " + name + " and I am " + age + " years old ");
+	}
+	void sayHello() {
+		System.out.println("hello there!");
+	}
+	int calculateYeardToRetirement() {
+		int yearsLeft = 65 - age;
+//		System.out.println(yearsLeft);
+		
+		return yearsLeft;
+	}
+}
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       
+		Person person1 = new Person();
+		person1.name = "Joe Bloggs";
+		person1.age = 37;
+		person1.speak();
+		//person1.sayHello();
+		int years = person1.calculateYeardToRetirement();	
+		
+		System.out.println("Years till retirement " + years);
+		Person person2 = new Person();
+		person2.name = "Sarah Smith";
+		person2.age = 20;
+		person2.speak();
+		//person2.calculateYeardToRetirement();
+		
+		//System.out.println(person1.name);
+		
+	}
+
+}
