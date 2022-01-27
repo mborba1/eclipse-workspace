a2bbd276b298ba1936e3c27d3fa0d2d648ea217b
Margareth Borba <mborba1@icloud.com>
2022-01-27 16:05:09 -0500

    tutorial9

---
+++ Tutorial9/src/App.java
@@ -0,0 +1,27 @@
+class Robot{
+	public void speak(String text) {
+		System.out.println(text);
+	}
+	public void jump(int height) {
+		System.out.println("Jumping: " + height);
+	}
+	public void move(String direction, double distance) {
+		System.out.println("Moving " + distance + " meters in direction " + direction);
+	}
+}
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Robot sam = new Robot();
+		
+		sam.speak("Hi I'm Sam");
+		sam.jump(7);
+		sam.move("West", 12.2);
+		String greeting = "Hello there";
+		
+		sam.speak(greeting);
+
+	}
+
+}
